b6c75f066968d055a660c4160a4db57e5cd6134e
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-08-25 21:11:37 -0400

    implemented styling for navbar, also reconfigured the way the navbar is being loaded in..

--- frontend/src/components/TopNavBar.tsx
+++ frontend/src/components/TopNavBar.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './TopNavBar.scss';
 
 const assets_path = "../assets/";
 const buttons = {
-  "names": ["Home", "About", "Discord", ],
-  "links": ["/", "/about", "https://discord.gg/558RfzrPNj", ],
-  "external": [0, 0, 1],
-  "images": ["home.png", "about.png", "discord.png"],
-  "left": [1, 0, 0],  
-  "width": [70, 50, 50]
+  "names": ["Home", "Physics Club", "Docs", "About", "Discord", ],
+  "links": ["/", 
+            "https://www.utm.utoronto.ca/cps/university-toronto-mississauga-physics-club",
+            "/docs", 
+            "/about", 
+            "https://discord.gg/558RfzrPNj"],
+  "external": [0, 1, 0, 0, 1],
+  "images": ["home.png", "club_logo_1.png", "docs.png", "about.png", "discord.png"],
+  "right": [0, 0, 1, 1, 1],  
+  "width": [110, 200, 100, 100, 100]
 };
+
+// the following button is used when the view width is less than 650px
+const buttonTray = {
+  // always goes to the right
+  "name": assets_path + "Tray",
+  "image": "tray.png",
+  "width": 100
+}
 
 // add the assets path to the images
 for (let i = 0; i < buttons.images.length; i++) {
@@ -18,38 +30,81 @@ for (let i = 0; i < buttons.images.length; i++) {
 }
 
 function TopNavBar() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [showMenu, setShowMenu] = useState(false);
 
-  const loadImage = (index: number) => {
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // some classic js dom listening
+    window.addEventListener('resize', handleResize);
+    return () => { // cleanup function
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  const loadLi = (index: number) => {
     return (
-      <img
-        src={buttons.images[index]}
-        alt={buttons.names[index]}
-        className="navImage"
-      />
+      <li key={index} 
+      style={{
+        width: buttons.width[index] + "px"
+      }}>
+          <img src={buttons.images[index]} 
+            alt={buttons.names[index]} 
+          />
+      </li>
     );
   }
+
+  // we have buttons on the left and right
+  const leftItems: JSX.Element[] = [];
+  const rightItems: JSX.Element[] = [];
+
+  buttons.names.forEach((_, index) => {
+    const item = buttons.external[index] === 1 ? ( // external link, use classic html anchors
+      <a href={buttons.links[index]}
+      key={index}
+      target="_blank"
+      rel="noopener noreferrer"
+      >{loadLi(index)}</a>
+    ) : ( // internal link, use react router
+      <NavLink to={buttons.links[index]}
+      key={index}
+      className="navLink"
+      >
+        {loadLi(index)}
+      </NavLink>
+    );
+
+    if (buttons.right[index]) {
+      rightItems.push(item);
+    } else {
+      leftItems.push(item);
+    }
+  });
 
   return (
     <nav className="topNavBar">
       <ul>
-        {buttons.names.map((name, index) => (
-            <li
-              className="navButton" 
-              key={index} 
-              style={{width: buttons.width[index] + "px"}}
+        {leftItems}
+        <div className="rightGroup">
+          { windowWidth > 650 ? rightItems : 
+            <div className="trayButton" onClick={toggleMenu}
+            style={{width: buttonTray.width + "px"}}
             >
-              {buttons.external[index] === 1 ? (
-                <a href={buttons.links[index]} 
-                target="_blank"
-                rel="noopener noreferrer"
-                >{loadImage(index)}</a>
-
-              ) : ( 
-                <NavLink to={buttons.links[index]}>{loadImage(index)}</NavLink>
-              )}
-            </li>
-          ))}
+              <img src={buttonTray.image} alt={buttonTray.name} />
+            </div>
+          }
+        </div>
       </ul>
+      { showMenu ? null : null }
     </nav>
   );
 }
