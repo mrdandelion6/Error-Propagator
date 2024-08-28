f4371b2380fcea18f3718169045bc123079801d9
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-08-27 22:32:01 -0400

    added tests for validating equations

--- frontend/src/__tests__/utils/verifyInputs.test.ts
+++ frontend/src/__tests__/utils/verifyInputs.test.ts
@@ -1,11 +1,52 @@
 import { validateEquation } from '../../utils/verifyInput';
 
 describe('validateEquation', () => {
+  // valid equations
   it('should return empty string for valid equations', () => {
     expect(validateEquation('x + y', ['x', 'y'])).toBe('');
   });
 
+  it('should treat values like pi and e as known variables', () => {
+    expect(validateEquation('x + pi', ['x'])).toBe('');
+    expect(validateEquation('x + e', ['x'])).toBe('');
+  });
+
+  it('should not have an issue with having variables in vars that are not in the equation', () => {
+    expect(validateEquation('x + y', ['x', 'y', 'z'])).toBe('');
+  });
+
+  // special functions
+  it('should not have an issue with trig functions', () => {
+    expect(validateEquation('sin(x^y^x^(3+x)) + cos(y)', ['x', 'y'])).toBe('');
+    expect(validateEquation('sin(x) + cos(y) + tan(z)', ['x', 'y', 'z'])).toBe('');
+  });
+
+  it('should not have an issue with log functions', () => {
+    expect(validateEquation('log(x) + ln(y)', ['x', 'y'])).toBe('');
+  });
+
+  // errors
   it('should return error message for unknown variables', () => {
     expect(validateEquation('x + y + z', ['x', 'y'])).toBe('Unknown variable: z');
+    expect(validateEquation('x + y + z + w', ['x', 'y'])).toBe('Unknown variables: z, w');
+  });
+
+  it('should return error message for variable name conflicts with known constants', () => {
+    expect(validateEquation('x + pi', ['x', 'pi'])).toBe('Variable name conflicts with known constants');
+  });
+
+  it('should return an error message when the equation unexpectedly ends', () => {
+    expect(validateEquation('x + y +', ['x', 'y'])).toBe('Unexpected end of equation');
+    expect(validateEquation('x + y *', ['x', 'y'])).toBe('Unexpected end of equation');
+  });
+
+  it('should specify missing brackets in the error message', () => {
+    expect(validateEquation('(x + y * z', ['x', 'y', 'z'])).toBe('Expected ) at [1:11]');
+    expect(validateEquation('x + y * z)', ['x', 'y', 'z'])).toBe('Expected end of equation at [1:11]');
+    expect(validateEquation('x + y * (z', ['x', 'y', 'z'])).toBe('Expected ) at [1:11]');
+    expect(validateEquation('x + (y * z', ['x', 'y', 'z'])).toBe('Expected ) at [1:11]');
+    expect(validateEquation('(x + (y * z', ['x', 'y', 'z'])).toBe('Expected ) at [1:12]');
+    expect(validateEquation('x + )y * z', ['x', 'y', 'z'])).toBe('Unexpected parenthesis');
+    expect(validateEquation(')x + y * z', ['x', 'y', 'z'])).toBe('Unexpected parenthesis');
   });
 });
--- frontend/src/utils/verifyInput.ts
+++ frontend/src/utils/verifyInput.ts
@@ -1,11 +1,58 @@
 import { Parser } from "expr-eval";
 
 export function validateEquation(eqn: string, vars: string[]): string {
+  /* 
+    This function is called by the ErrorPropagator component to validate the equation entered by the user.
+    It uses the expr-eval library to parse the equation and check for unknown variables.
+    The function returns an error message if there are unknown variables or if the equation is invalid.
+    The error message is displayed to the user in the ErrorPropagator component.
+  */
+
+  const clean = (message: string): string => {
+    // console.log(message);
+
+    if (message === "") {
+      console.log('Unexpected empty string for error message');
+      return message;
+    }
+
+    if (message.startsWith('parse error') && message.includes('Expected')) {
+      const words = message.split(' ');
+      message=words.slice(words.indexOf('Expected')).join(' ');
+      const errorLocation = words[words.indexOf('Expected') -1].slice(0, -1);
+      if ( errorLocation.endsWith(']') && errorLocation.startsWith('[') ) {
+        message += ' at ' + errorLocation;
+      }
+    }
+    const replaceItems: {[key: string]: string} = {
+      'EOF' : "end of equation",
+      'TPAREN' : "parenthesis",
+    }
+    for (const x in replaceItems) {
+      if (Object.prototype.hasOwnProperty.call(replaceItems, x)) {
+        message = message.replace(new RegExp(x, 'g'), replaceItems[x]);
+        
+        if (x == 'TPAREN')  {
+          message = message.replace(/[:\s(]+$/, '');
+          message = message.replace(/[:\s)]+$/, '');
+        }
+      }
+    }
+    message = message.trim();
+    message = message.charAt(0).toUpperCase() + message.slice(1);
+    return message;
+  }
+
+  const constants = ['pi', 'e'];
+  if (vars.some(v => constants.includes(v))) {
+    return 'Variable name conflicts with known constants';
+  }
+  vars = vars.concat(constants); // add constants to known variables
   const p = new Parser();
   try {
     const parsed = p.parse(eqn);
-    const variables = parsed.variables();
-    const unknownVars = variables.filter((v: string) => !vars.includes(v));
+    const enteredVariables = parsed.variables();
+    const unknownVars = enteredVariables.filter((v: string) => !vars.includes(v));
     if (unknownVars.length === 1) {
       return `Unknown variable: ${unknownVars[0]}`;
     }
@@ -14,6 +61,20 @@ export function validateEquation(eqn: string, vars: string[]): string {
     }
     return ''; // no errors
   } catch (error) {
-    return '';
+    if (error instanceof Error) {
+      let message: string = error.message;
+      // console.log(message);
+
+      switch (message) {
+        case "unexpected TEOF: EOF":
+          return 'Unexpected end of equation';
+        default:
+          message = clean(message);
+          return message;
+      }
+    
+    } else {
+      return 'An unknown error occurred';
+    }
   }
 }
