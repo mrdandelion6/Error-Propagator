841bf685edad5d4dac13f5bd6a94410a6f59730d
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-11-12 01:17:30 -0500

    added sqlite3 database back in settings.py so we can run tets with TestCase from django.test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,7 +74,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {}
+# need for running tests
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Make sure BASE_DIR is defined at the top of settings.py
+    }
+}
 
 
 # Password validation
