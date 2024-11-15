df5a8f7758757f9b32c45c36cb1b39f215964d43
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-11-15 18:46:15 -0500

    remove unused React import

--- frontend/src/components/error_propagator/ErrorPropagator.tsx
+++ frontend/src/components/error_propagator/ErrorPropagator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ValueBox from './ValueBox'
 import EquationBox from './EquationBox'
 import ResultBox from './ResultBox'
