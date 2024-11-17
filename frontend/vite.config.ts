d5132eb594f36324bf06116c4b80627f1603ea5e
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-11-16 19:34:22 -0500

    now using ghub secret keys for frontend prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
-    import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: '/',
-  server: { // create proxy for backend
+  
+  server: { // only used in development
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
@@ -17,7 +18,12 @@ export default defineConfig({
 
   resolve: {
     preserveSymlinks: true
+  },
+
+  // add environment variables definition
+  define: {
+    __API_URL__: mode === 'production' 
+      ? JSON.stringify(process.env.VITE_LAMBDA_API_URL)
+      : JSON.stringify('http://127.0.0.1:8000'),
   }
-})
-
-
+}))
