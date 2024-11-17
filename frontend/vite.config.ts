c259df79500566a2a27d24d17fa35f9a645ade26
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-11-16 19:47:29 -0500

    added log for testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,23 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: '/',
-  
-  server: { // only used in development
-    proxy: {
-      '/api': {
-        target: mode === 'production'
-          ? process.env.VITE_LAMBDA_API_URL
-          : 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+export default defineConfig(({ mode }) => {
+  const proxyTarget = mode === 'production'
+    ? process.env.VITE_LAMBDA_API_URL
+    : 'http://127.0.0.1:8000';
+
+  console.log('Mode:', mode);
+  console.log('Proxy Target:', proxyTarget);
+
+  return {
+    plugins: [react()],
+    base: '/',
+    
+    server: {
+      proxy: {
+        '/api': {
+          target: proxyTarget,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
       },
     },
-  },
 
-  resolve: {
-    preserveSymlinks: true
+    resolve: {
+      preserveSymlinks: true
+    }
   }
-}))
+})
